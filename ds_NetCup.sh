6fef5bd12fe4d259547c5654724fec8d22245a0c
0xZhoueth <100511342+0xZhoueth@users.noreply.github.com>
2024-04-29 15:56:39 +0800

    Update ds_NetCup.sh

--- ds_NetCup.sh
+++ ds_NetCup.sh
@@ -1,17 +1,43 @@
 #!/bin/bash
 
 # 格式化磁盘
-sudo mkfs.ext4 /dev/vdb
+if sudo mkfs.ext4 /dev/vdb; then
+    echo "磁盘格式化成功"
+else
+    echo "磁盘格式化失败"
+    exit 1
+fi
 
 # 挂载分区
-sudo mount /dev/vdb /mnt/newdisk
+if sudo mount /dev/vdb /mnt/newdisk; then
+    echo "分区挂载成功"
+else
+    echo "分区挂载失败"
+    exit 1
+fi
 
 # 开机自动挂载分区
 # 第一步：获取磁盘的uuid
 disk_uuid=$(sudo blkid -s UUID -o value /dev/vdb)
+if [ -z "$disk_uuid" ]; then
+    echo "获取磁盘UUID失败"
+    exit 1
+else
+    echo "磁盘UUID获取成功：$disk_uuid"
+fi
 
 # 第二步：编辑文件
-sudo sh -c 'echo "UUID='"$disk_uuid"' /mnt/newdisk ext4 defaults 0 2" >> /etc/fstab'
+if sudo sh -c 'echo "UUID='"$disk_uuid"' /mnt/newdisk ext4 defaults 0 2" >> /etc/fstab'; then
+    echo "成功将UUID写入 /etc/fstab"
+else
+    echo "写入 /etc/fstab 失败"
+    exit 1
+fi
 
 # 第四步：重新挂载所有分区，以验证/etc/fstab配置是否正确。
-sudo mount -a
+if sudo mount -a; then
+    echo "重新挂载分区成功"
+else
+    echo "重新挂载分区失败"
+    exit 1
+fi
